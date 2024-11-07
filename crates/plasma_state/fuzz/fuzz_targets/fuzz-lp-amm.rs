be137ebb12ab1ca341a5b1324a08a7065d11e63f
Jarry Xiao <jarry.xiao@gmail.com>
2024-11-07 12:24:06 -0500

    Add new fuzz target

---
+++ crates/plasma_state/fuzz/fuzz_targets/fuzz-lp-amm.rs
@@ -0,0 +1,20 @@
+#![no_main]
+use libfuzzer_sys::fuzz_target;
+use plasma_fuzz::*;
+use plasma_state::amm::Amm;
+
+fuzz_target!(|actions: (u32, Vec<LpAction>)| {
+    let (fee, actions) = actions;
+    let mut amm = Amm::new(fee % 26, 5, 4, 0);
+    let base = 279_900_000_000_000;
+    let quote = 100_000_000_000;
+    let mut lps = vec![None; 20];
+
+    let lp_shares = ((base as f64) * (quote as f64)).sqrt() as u64;
+    amm.mint(0, base, quote, Some(lp_shares)).unwrap();
+
+    // fuzzed code goes here
+    for action in actions {
+        perform_lp_action(&mut amm, &mut lps, action);
+    }
+});
